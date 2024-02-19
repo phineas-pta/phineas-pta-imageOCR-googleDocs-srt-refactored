9b52fa9f669e6e929ba358ec8e8d9c446efd660a
Phan Tuấn Anh <phantuananh@hotmail.com.vn>
2024-02-19 13:11:31 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# How to use
-* [Install python](https://www.python.org/)
-* Create new project and download credentials.json file from [Python Quickstart](https://developers.google.com/drive/api/v3/quickstart/python)
-* put credentials.json file beside main.py
-* Install the Google Client Library using
-```
-pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-```
-* Export images using [VideoSubFinder](https://sourceforge.net/projects/videosubfinder/) and put them in images folder
-* Run main.py and login with google account (only for first time)
-* Wait until it complete processing evey image.
+# Image OCR using google docs, output to srt file
+
+refactor the code more properly
+
+## How to use
+
+Create new google cloud project and download `credentials.json` file: see https://developers.google.com/drive/api/quickstart/python
+
+put credentials.json file beside main.py
+
+Install the Google Client Library: `pip install -r requirements.txt`
+
+Export images fro hard-subbed video: https://sourceforge.net/projects/videosubfinder/
+
+Run `python main.py` and login with google account (only for first time), it creates a new file `token.json`
+
+Wait until it complete processing evey image.
+
+Code is based on https://tanaikech.github.io/2017/05/02/ocr-using-google-drive-api/
--- main.py
+++ main.py
@@ -1,144 +1,101 @@
-from __future__ import print_function
-import httplib2
 import os
 import io
-
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-from apiclient.http import MediaFileUpload, MediaIoBaseDownload
-from pathlib import Path
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-# Code is based on https://tanaikech.github.io/2017/05/02/ocr-using-google-drive-api/
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive'
-CLIENT_SECRET_FILE = 'credentials.json'
-APPLICATION_NAME = 'Drive API Python Quickstart'
+import srt
+from glob import glob
+from tqdm import tqdm
+from datetime import timedelta
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient import discovery
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    credential_path = os.path.join("./", 'token.json')
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+def get_credentials(token_file, credentials_file):
+	if os.path.exists(token_file):
+		creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+	else:
+		creds = None
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
+			creds = flow.run_local_server(port=0)
+		with open(token_file, "w") as token:
+			token.write(creds.to_json())
+		print("Storing credentials to", token_file)
+	return creds
 
 
-def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
+CREDENTIALS = get_credentials("token.json", "credentials.json")
+SERVICE = discovery.build(serviceName="drive", version="v3", credentials=CREDENTIALS)
+MIME = "application/vnd.google-apps.document"
 
-    # imgfile = 'image.jpeg'  # Image with texts (png, jpg, bmp, gif, pdf)
-    # txtfile = 'text.txt'  # Text file outputted by OCR
-
-    current_directory = Path(Path.cwd())
-    images_dir = Path(f'{current_directory}/images')
-    raw_texts_dir = Path(f'{current_directory}/raw_texts')
-    texts_dir = Path(f'{current_directory}/texts')
-    srt_file = open(Path(f'{current_directory}/subtitle_output.srt'), 'a', encoding='utf-8')
-    line = 1
-
-    # check directory if exists
-    if not images_dir.exists():
-        images_dir.mkdir()
-        print('Images folder is empty.')
-        exit()
-
-    if not raw_texts_dir.exists():
-        raw_texts_dir.mkdir()
-    if not texts_dir.exists():
-        texts_dir.mkdir()
-
-    images = Path(f'{current_directory}/images').rglob('*.jpeg')
-    for image in images:
-
-        # Get data
-        imgfile = str(image.absolute())
-        imgname = str(image.name)
-        raw_txtfile = f'{current_directory}/raw_texts/{imgname[:-5]}.txt'
-        txtfile = f'{current_directory}/texts/{imgname[:-5]}.txt'
-
-        mime = 'application/vnd.google-apps.document'
-        res = service.files().create(
-            body={
-                'name': imgname,
-                'mimeType': mime
-            },
-            media_body=MediaFileUpload(imgfile, mimetype=mime, resumable=True)
-        ).execute()
-
-        downloader = MediaIoBaseDownload(
-            io.FileIO(raw_txtfile, 'wb'),
-            service.files().export_media(fileId=res['id'], mimeType="text/plain")
-        )
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-
-        service.files().delete(fileId=res['id']).execute()
-
-        # Create clean text file
-        raw_text_file = open(raw_txtfile, 'r', encoding='utf-8')
-        text_content = raw_text_file.read()
-        raw_text_file.close()
-        text_content = text_content.split('\n')
-        text_content = ''.join(text_content[2:])
-        text_file = open(txtfile, 'w', encoding='utf-8')
-        text_file.write(text_content)
-        text_file.close()
-
-        start_hour = imgname.split('_')[0][:2]
-        start_min = imgname.split('_')[1][:2]
-        start_sec = imgname.split('_')[2][:2]
-        start_micro = imgname.split('_')[3][:3]
-
-        end_hour = imgname.split('__')[1].split('_')[0][:2]
-        end_min = imgname.split('__')[1].split('_')[1][:2]
-        end_sec = imgname.split('__')[1].split('_')[2][:2]
-        end_micro = imgname.split('__')[1].split('_')[3][:3]
-
-        # Format start time
-        start_time = f'{start_hour}:{start_min}:{start_sec},{start_micro}'
-
-        # Format end time
-        end_time = f'{end_hour}:{end_min}:{end_sec},{end_micro}'
-        # Append the line to srt file
-        srt_file.writelines([
-            f'{line}\n',
-            f'{start_time} --> {end_time}\n',
-            f'{text_content}\n\n',
-            ''
-        ])
-
-        line += 1
-
-        print(f"{imgname} Done.")
-
-    srt_file.close()
+IMAGES_DIR = "RGBImages"
+RAW_TEXTS_DIR = "TXTResults"
+images = glob("*.jpeg", root_dir=IMAGES_DIR)
+if not os.path.isdir(IMAGES_DIR) or len(images) == 0: raise ValueError("empty image folder")
+if not os.path.isdir(RAW_TEXTS_DIR): os.mkdir(RAW_TEXTS_DIR)
 
 
-if __name__ == '__main__':
-    main()
+def get_text_from_ocr(image: srt) -> str:
+	"""use google docs OCR"""
+	imgfile = os.path.join(IMAGES_DIR, image)
+	imgname = os.path.splitext(image)[0]
+	raw_txtfile = os.path.join(RAW_TEXTS_DIR, imgname+".txt")
+
+	res = SERVICE.files().create(
+		body={"name": imgfile, "mimeType": MIME}, fields="id",
+		media_body=MediaFileUpload(imgfile, mimetype=MIME, resumable=True)
+	).execute()
+	file_id = res.get("id")
+
+	downloader = MediaIoBaseDownload(
+		io.FileIO(raw_txtfile, "wb"),
+		SERVICE.files().export_media(fileId=file_id, mimeType="text/plain")
+	)
+	done = False
+	while not done:
+		_, done = downloader.next_chunk()
+	SERVICE.files().delete(fileId=file_id).execute()
+
+	with open(raw_txtfile, "r", encoding="utf-8") as raw_text_file:
+		text_content = raw_text_file.read().split("\n")
+	return "".join(text_content[2:])
+
+
+def get_timestamp_from_filename(image: srt) -> tuple[timedelta, timedelta]:
+	"""get start & end timestamp from image file name, which comes from VideoSubFinder"""
+	imgname = os.path.splitext(image)[0]
+
+	tmp_start = imgname.split("_")
+	start_hour  = int(tmp_start[0][:2])
+	start_min   = int(tmp_start[1][:2])
+	start_sec   = int(tmp_start[2][:2])
+	start_micro = int(tmp_start[3][:3])
+
+	tmp_end = imgname.split("__")[1].split("_")
+	end_hour  = int(tmp_end[0][:2])
+	end_min   = int(tmp_end[1][:2])
+	end_sec   = int(tmp_end[2][:2])
+	end_micro = int(tmp_end[3][:3])
+
+	start_time = timedelta(hours=start_hour, minutes=start_min, seconds=start_sec, microseconds=start_micro)
+	end_time   = timedelta(hours=  end_hour, minutes=  end_min, seconds=  end_sec, microseconds=  end_micro)
+	return start_time, end_time
+
+
+#################################### main #####################################
+
+OUTPUT_FILE = "subtitle_output.srt"
+subs = []
+
+for line, image in enumerate(tqdm(images, unit="image"), start=1):
+	txt = get_text_from_ocr(image)
+	start_time, end_time = get_timestamp_from_filename(image)
+	subs.append(srt.Subtitle(index=line, start=start_time, end=end_time, content=txt))
+
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+	f.write(srt.compose(srt.sort_and_reindex(subs)))  # order get messed up on linux
